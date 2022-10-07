982a0c2de19fcc13c86bfcc724d5ba6a1848378e
Eric Whale <hsh048148@gmail.com>
2022-10-07 12:34:01 +0900

    Add #setuser function

--- client.py
+++ client.py
@@ -7,53 +7,48 @@ def main():
     socket.connect("tcp://localhost:5557")
 
     print("Welcome to pyzChat!")
-    print(">>> Login")
-    for i in range(3):
-        id = input("user id: ")
-        pwd = input("password: ")
-        socket.send_string(f"#login {id} {pwd}")
-        status = socket_recv_string()
-
-    print("Enter #help for commands")
+    username = input("Set your username : ")
+    socket.send_string(f"#setuser {username}")
+    user = socket.recv_json()
+    print(user)
     while True:
-        requestMsg = input('Command : ')
-        if requestMsg == "#help":
+        print("Enter #help for commands")
+        message = input(">>> ")
+        if message[0] == "#":
+            if message == "#quit":
+                print("Exit pyzChat...")
+                break
+            commands(socket, message)
+        else:
+            socket.send_string(message)
+            reply = socket.recv_string()
+            print(reply)
+
+def commands(socket, message):
+        # HELP
+        if message == "#help":
             print("Enter #chatrooms for chatrooms")
             print("Enter #refresh or #re to refresh chatrooms")
-
-
-        # Chatroom related funcitons
         # SHOW chatrooms
-        elif (requestMsg[:3] == "#re") or (requestMsg == "#chatrooms"):
+        elif (message[:3] == "#re") or (message == "#chatrooms"):
             socket.send_string("#chatrooms")
             reply = socket.recv_string()
             print(f"chatroom list : \n{reply}")
         # ADD chatroom
-        elif requestMsg == "#addroom":
+        elif message == "#addroom":
             room_name = input("Room name(no spaces) : ")
             socket.send_string("#addroom " + room_name)
             reply = socket.recv_string()
             print(f"chatroom list : \n{reply}")
-        elif requestMsg == "#removeroom":
+        elif message == "#removeroom":
             room_name = input("Room name(no spaces) : ")
             # remove room only if the user is the host
-
-        elif requestMsg == "#enterroom":
+        elif message == "#enterroom":
             room_name = input("Room name : ")
             print(f"Entering the room {room_name}")
             # enter the room
-            
-        elif requestMsg == "#quit":
-            print("Exit pyzChat...")
-            break
 
 
 if __name__ == '__main__':
-    # while True:
-    #     print("LOGO")
-    #     print("Welcome to pyzchat!")
-    #     print("Enter `#refresh` or `#re` if you don't see a room you want to join, Enter `#help` for any other help.")
-    #     print("<Chatrooms>")
-    #     break
     main()
 
--- server.py
+++ server.py
@@ -10,19 +10,6 @@ class Chatroom:
     def join(self):
         pass
 
-class User:
-    def __init__(self, name) -> None:
-        self.name = name
-        self.id = "345r34e34"    # TODO
-        self.pwd = "1234"
-
-tom = User("tom")
-eric = User("eric")
-joe = User("joe")
-
-users = [tom, eric, joe]
-username = {tom.name, eric.name, joe.name}
-userpwd = {tom.pwd, eric.pwd, joe.pwd}
 chatrooms = ['lion', 'tiger', 'leopard']
 
 def main():
@@ -32,27 +19,26 @@ def main():
     socket.bind("tcp://*:5557")
     while True:
         request = socket.recv_string()
-
-        if request[:6] == "#login":
-            login_info = request.split()
-            id = login_info[1]
-            pwd = login_info[2]
-            if id in username and pwd in userpwd:
-                # Grant Access
-                pass
-                
-        if request == "#chatrooms":
-            rooms_str = '\n'.join(chatrooms)
-            socket.send_string(rooms_str)
-        elif request[:8] == "#addroom":
-            room_name = request.split()[1]
-            chatrooms.append(room_name)
-            rooms_str = '\n'.join(chatrooms)
-            socket.send_string(rooms_str)
+        if request[0] == "#":
+            if request[:8] == "#setuser":
+                name = request.split()[1]
+                new_user = {"name": name, "id": 1234}
+                # Add new_user in DB
+                # if adding succeed:
+                socket.send_json(new_user)
+            elif request == "#chatrooms":
+                rooms_str = '\n'.join(chatrooms)
+                socket.send_string(rooms_str)
+            elif request[:8] == "#addroom":
+                room_name = request.split()[1]
+                chatrooms.append(room_name)
+                rooms_str = '\n'.join(chatrooms)
+                socket.send_string(rooms_str)
+        else:
+            socket.send_string(f"{request} wow")
 
 
 if __name__ == '__main__':
     main()
 
 
-# TODO: use django?(web-framework for chatrooms and zmq for a chatroom?)

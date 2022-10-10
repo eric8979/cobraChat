f3cb19b2b45718353e97316483bb8adaeced4fe8
Eric Whale <hsh048148@gmail.com>
2022-10-10 20:32:39 +0900

    Setup basic structure for chatting

---
+++ chatroom/server.py
@@ -0,0 +1,18 @@
+# pubsub with pipeline 
+# e.g. chatting app
+import zmq
+
+def main():
+    ctx = zmq.Context()
+    publisher = ctx.socket(zmq.PUB)
+    publisher.bind("tcp://*:5557")
+    collector = ctx.socket(zmq.PULL)
+    collector.bind("tcp://*:5558")
+
+    while True:
+        message = collector.recv()
+        print("server: publishing update => ", message)
+        publisher.send(message)
+
+if __name__ == '__main__':
+    main()
